6c9de8c8e28c3b7d5fface9ecbeb32cde393f478
Charles Zou <guixiz@microsoft.com>
2015-12-13 19:24:57 -0800

    solution for leetcode292

---
+++ LeetCode292.java
@@ -0,0 +1,20 @@
+/*Nim Game My Submissions Question
+You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+
+Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.
+
+For example, if there are 4 stones in the heap, then you will never win the game: no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
+*/
+
+public class Solution {
+    public boolean canWinNim(int n) {
+        // we need to find out the winning or lossing postion, since each player has to take 1 --3 stones
+        // we know all the state which has num % 4 == 0 are loser postion, as no matter what we do, we're stuck in the loser postion
+        
+        if(n % 4 == 0){
+            return false;
+        }
+        
+        return true;
+    }
+}
