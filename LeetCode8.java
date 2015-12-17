8c6df7bcf7bd385b694fba988efee62118f9e861
Charles Zou <guixiz@microsoft.com>
2015-12-17 01:25:33 -0800

    Solution for LeetCode 8

---
+++ LeetCode8.java
@@ -0,0 +1,80 @@
+/*
+String to Integer (atoi) My Submissions Question
+Implement atoi to convert a string to an integer.
+
+Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+
+Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+
+Update (2015-02-10):
+The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
+
+spoilers alert... click to show requirements for atoi.
+
+Requirements for atoi:
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+*/
+
+public class Solution {
+    public int myAtoi(String s) {
+        
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        
+        int i = 0;
+        while(i < s.length() && s.charAt(i) == ' '){
+            i++;
+        }
+        
+        if(i == s.length()) return 0;
+        boolean isNegative = false;
+        if(s.charAt(i)=='-') {
+            isNegative = true;
+            i++;
+        }
+        if( i < s.length() && s.charAt(i)=='+')
+        {
+            if(isNegative)
+            {
+                return 0;
+            }
+            i++;
+            if( i < s.length() && s.charAt(i) == '-')
+            {
+                return 0;
+            }
+        }
+        while(i < s.length() && s.charAt(i) == '0')
+        {
+            i++;
+        }
+        
+        long ans = 0;
+        while( i < s.length())
+        {
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch))
+            {
+                ans = ans * 10 + s.charAt(i++) - '0';
+                if(ans > Integer.MAX_VALUE)
+                {
+                    return isNegative?Integer.MIN_VALUE : Integer.MAX_VALUE;
+                }
+            }else{
+                break;
+            }
+                
+        }
+          if(isNegative){
+              return -(int)ans;
+          }
+          return (int)ans;
+     }
+}
