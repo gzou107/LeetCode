ba91b7c1e92ffb6cbff7ac423adc0063697df25b
Charles Zou <guixiz@microsoft.com>
2016-01-19 00:40:13 -0800

    Solution for LeetCode 70 and 80

--- LeetCode80.java
+++ LeetCode80.java
@@ -1,29 +1,44 @@
 /*
-Climbing Stairs My Submissions Question
-You are climbing a stair case. It takes n steps to reach to the top.
+80. Remove Duplicates from Sorted Array II My Submissions Question
+Total Accepted: 63627 Total Submissions: 200523 Difficulty: Medium
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
 
-Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 */
 
+
 public class Solution {
-    public int climbStairs(int n) {
-        // f(n) =  f(n - 1) + f(n-2);
+    public int removeDuplicates(int[] nums) {
+        return help(nums);
+    }
+    
+    private int help(int [] nums)
+    {
+        if(nums == null) return 0;
         
-        if(n <= 0) return 0;
-        if(n <= 2) return n;
-        
-        int fN_2 = 1;
-        int fN_1 = 2;
-        int fN = 0;
-        
-        int i = 2;
-        while( i < n){
-            fN = fN_2 + fN_1;
-            fN_2 = fN_1;
-            fN_1 = fN;
-            i++;
+        int dStart = 0;
+        int dEnd = 0;
+        int len = 0;
+        while(dEnd < nums.length)
+        {
+            if(nums[dEnd] == nums[dStart])
+            {
+                if(dEnd - dStart + 1 <=2)
+                {
+                   nums[len++] = nums[dEnd]; 
+                }else{
+                    // does nothing here
+                }
+            }else{
+                nums[len++] = nums[dEnd];
+                dStart = dEnd;
+            }
+            dEnd++;
         }
-        
-        return fN;
+        return len;
     }
 }
