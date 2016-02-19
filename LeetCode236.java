eaf32844a72afa7a86e327b40d2b4c6d3d9dc396
Charles Zou <guixiz@microsoft.com>
2016-02-19 01:27:58 -0800

    refactoring and add more methods for lca. tableau phone interview questions.

--- LeetCode236.java
+++ LeetCode236.java
@@ -45,3 +45,142 @@ public class Solution {
         return l == null? r : l;
     }
 }
+
+/**
+2016/01/17 Tableau phone interview !!!
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //return help0(root, p, q);
+        //return help1(root, p, q);
+        return help2(root, p, q);
+    }
+    
+    private TreeNode help0(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if(root == null || p == null || q == null) return null;
+        if(root == p || root == q) return root;
+        
+        TreeNode l = help0(root.left, p, q);
+        TreeNode r = help0(root.right, p, q);
+        
+        if(l != null && r != null) return root;
+        
+        return l == null? r : l;
+    }
+    
+    private TreeNode help1(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if(root == null) return null;
+        if(p == null) return q;
+        if(q == null) return p;
+        
+        List<TreeNode>p1 = new ArrayList<>();
+        List<TreeNode>p2 = new ArrayList<>();
+        
+        findPath(root, p, new ArrayList<TreeNode>(), p1);
+        findPath(root, q, new ArrayList<TreeNode>(), p2);
+        
+        //  now we find the longest common prefix of these two
+        int i = 0;
+        for( i = 0; i <p1.size() && i < p2.size(); i++)
+        {
+            if(p1.get(i) != p2.get(i)){
+                return p1.get(i-1);
+            }
+        }
+        if(i == p1.size()){
+            return p1.get(p1.size()-1);
+        }
+        
+        if( i == p2.size()){
+            return p2.get(p2.size()-1);
+        }
+        return null;
+    }
+    
+    private void findPath(TreeNode root, TreeNode p, List<TreeNode>temp, List<TreeNode>path)
+    {
+        if(root == p){
+            for(TreeNode t : temp){
+                path.add(t);
+            }
+            path.add(root);
+            return;
+        }    
+        temp.add(root);
+        if(root.left != null){
+            findPath(root.left, p, temp, path);
+        }
+        
+        if(root.right != null){
+            findPath(root.right, p, temp, path);
+        }
+        temp.remove(temp.size()-1);
+    }
+    
+    private TreeNode help2(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if(root == null) return null;
+        if(p == null) return q;
+        if(q == null) return p;
+   
+        wrapperNode res = dfs(root, p, q);
+        
+        return res.node;
+    }
+    private wrapperNode dfs(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if(root == null) return new wrapperNode(null, 0);
+        if(root == p || root == q) return new wrapperNode(root, 1);
+        
+        wrapperNode l =  dfs(root.left, p, q);
+        wrapperNode r = dfs(root.right, p, q);
+        
+        
+        if( l.count == 2) return l;
+        if( r.count == 2) return r;
+        
+        if(l.count + r.count == 2){
+            return new wrapperNode(root, 2);
+        }
+        
+        if(l.count == 1){
+            if(root == p || root == q){
+             return new wrapperNode(root, 2);
+            }else{
+                return new wrapperNode(l.node, 1);
+            }
+        }
+        
+        if( r.count == 1){
+            if(root == p || root == q){
+             return new wrapperNode(root, 2);
+            }else{
+                return new wrapperNode(r.node, 1);
+            }
+        }
+        
+        return new wrapperNode(null, 0);
+    }
+    
+    private class wrapperNode 
+    {
+        public TreeNode node;
+        public int count;
+        
+        public wrapperNode(TreeNode t, int c)
+        {
+            this.node = t;
+            this.count = c;
+        }
+    }
+}
