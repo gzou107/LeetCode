9266c99793ab9271f95a5cf0a9ab8efe54fc6667
Charles Zou <guixiz@microsoft.com>
2016-03-14 23:48:59 -0700

    Solution for leetcode 12

--- LeetCode12.java
+++ LeetCode12.java
@@ -1,36 +1,47 @@
 /*
-Roman to Integer My Submissions Question
-Given a roman numeral, convert it to an integer.
+12. Integer to Roman My Submissions Question
+Total Accepted: 58622 Total Submissions: 154367 Difficulty: Medium
+Given an integer, convert it to a roman numeral.
 
 Input is guaranteed to be within the range from 1 to 3999.
 */
 
 public class Solution {
-    public int romanToInt(String s) {
-        if( s == null || s.length() == 0){
-            return 0;
+    public String intToRoman(int num) {
+       return help1(num); 
+    }
+    
+    private int []numbers =    {1000, 900, 500,400, 100, 90, 50, 40, 10,  9,  5,   4,   1};
+    private String [] romans = {"M", "CM","D","CD","C","XC","L","XL","X","IX","V","IV", "I"};
+    
+    private String help(int num)
+    {
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < numbers.length && num > 0; i++)
+        {
+            while(num >= numbers[i])
+            {
+                result.append(romans[i]);
+                num -= numbers[i];
+            }
         }
-        
-       Map<Character, Integer> mapping = new HashMap<Character, Integer>();
-       mapping.put('I',1);
-       mapping.put('V',5);
-       mapping.put('X',10);
-       mapping.put('L',50);
-       mapping.put('C',100);
-       mapping.put('D',500);
-       mapping.put('M',1000);
-       // starting from least significant position
-       int ans = mapping.get(s.charAt(s.length()-1));
-       
-       for(int i = s.length()-2; i>=0; i--){
-           // direct translation of meaning of C, X, I
-           if(mapping.get(s.charAt(i+1)) > mapping.get(s.charAt(i))){
-               ans -= mapping.get(s.charAt(i));
-           }else{
-               ans += mapping.get(s.charAt(i));
-           }
-       }
-       
-       return ans;
+        return result.toString();
+    }
+    
+    private String help1(int num)
+    {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while(num > 0)
+        {
+            int k = num/numbers[i];
+            for(int j = 0; j < k; j++)
+            {
+                result.append(romans[i]);
+                num -= numbers[i];
+            }
+            i++;
+        }
+        return result.toString();
     }
 }
